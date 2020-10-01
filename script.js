b5f686f49a4270ae621a04342e2d59700ebfcc71
rrrossettiii <rrrossettiii@gmail.com>
2020-10-01 18:03:37 -0400

    Edits to the CSS, INDEX and added reset button and functions to JS

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
-// Assignment Code
-function pwPrompts() {
-	// Base character imput.
-	var abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	// console.log(abc);
+// PASSWORD GENERATOR
+//
 
-	// Prompt - length of password
-	var pwLength = prompt(
-		"How many characters long whould you like your password to be?",
-		"Pick a number between 8-128."
-	);
+// Generate Button
+//
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePW);
 
-	// Keeps password length between 8-128.
-	while (pwLength < 7 || pwLength > 129) {
-		alert("Please select a number between 8-128!");
+// This function runs all of the prompts and the generator itself and it writes the code to the textfield in index.html
+//
+function generatePW() {
+	if (typeof passArray === "undefined") {
+		// Prompt - length of password
+		//
 		var pwLength = prompt(
 			"How many characters long whould you like your password to be?",
 			"Pick a number between 8-128."
 		);
-	}
 
-	// Prompt - use numbers?
-	if (confirm("Would you like to use numbers?") === true) {
-		var num = "0123456789";
-	}
-	// console.log(num);
+		// Prompt - loop that keeps password length between 8-128.
+		//
+		while (pwLength < 7 || pwLength > 129) {
+			alert("Please select a number between 8-128!");
+			var pwLength = prompt(
+				"How many characters long whould you like your password to be?",
+				"Pick a number between 8-128."
+			);
+		}
 
-	// Prompt - use symbols?
-	if (confirm("would you like to use special characters?") === true) {
-		var sym = ' !#$%&()*+,-./:;<=>?@[]^_`{|}"~';
+		// Prompt - use numbers?
+		//
+		if (confirm("Would you like to use numbers?") === true) {
+			var num = "0123456789";
+		}
+
+		// Prompt - use symbols?
+		//
+		if (confirm("would you like to use special characters?") === true) {
+			var sym = ' !#$%&()*+,-./:;<=>?@[]^_`{|}"~';
+		}
+
+		// Base character string.
+		//
+		var abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		// All selected character strings put together
+		//
+		characters = abc.concat(num, sym);
+
+		// Password length and character string combined.
+		//
+		passArray = [pwLength, characters];
 	}
-	// console.log(sym);
 
 	// The Password Generator
+	//
 	function generatePassword(length) {
 		var password = "";
-		var characters = abc.concat(num, sym);
-		var charactersLength = characters.length;
+		var charactersLength = passArray[1].length;
 		for (var i = 0; i < length; i++) {
 			password += characters.charAt(
 				Math.floor(Math.random() * charactersLength)
 			);
 		}
+		document.getElementById("reset").style.backgroundColor = "orange";
 		return password;
 	}
-	// console.log(generatePassword(pwLength));
 
-	// Writes the password to the 'password' id in index.html
-	var password = generatePassword(pwLength);
+	// Writes the generated password to the 'password' id in index.html
+	//
+	var password = generatePassword(passArray[0]);
 	var passwordText = document.querySelector("#password");
 	passwordText.value = password;
 }
 
-// Creates the Generate button.
-var generateBtn = document.querySelector("#generate");
-// Event listener for the Generate button.
-generateBtn.addEventListener("click", pwPrompts);
+// Reset Button - reloads page & replaces text field
+//
+var resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", reload);
+function reload() {
+	document.querySelector(".placeholder").value = "";
+	location.reload();
+	return false;
+}
